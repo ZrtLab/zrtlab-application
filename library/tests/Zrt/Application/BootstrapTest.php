f6ac20e6f6b81d406e5428c09ebc34eebed9c363
Luis Alberto Mayta Mamani <slovacus@gmail.com>
2014-05-26 15:47:34 -0500

    fixed name class Bootstrap

--- library/tests/Zrt/Application/BootstrapTest.php
+++ library/tests/Zrt/Application/BootstrapTest.php
@@ -2,7 +2,7 @@
 
 namespace Zrt\Tests\Application;
 
-use Zrt_Application_Boostrap_Boostrap,
+use Zrt_Application_Bootstrap_Bootstrap,
     PHPUnit_Framework_TestCase,
     Zend_Controller_Front,
     Zend_Registry,
@@ -14,7 +14,7 @@ class BoostrapTest extends PHPUnit_Framework_TestCase
     public function setUp()
     {
         $this->application = new Zend_Application('testing',$_SERVER['config_path']);
-        $this->bootstrap = new Zrt_Application_Boostrap_Boostrap($this->application);
+        $this->bootstrap = new Zrt_Application_Bootstrap_Bootstrap($this->application);
         Zend_Controller_Front::getInstance()->resetInstance();
     }
 
