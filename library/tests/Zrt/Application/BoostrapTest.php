d3ff9df23d7ddfe53b14c148a32e5cb9ab1ac247
Luis Alberto Mayta Mamani <slovacus@gmail.com>
2014-05-26 12:32:52 -0500

    added class BoostrapTest
    clase la cual se va a generar el inicio de la libreria

---
+++ library/tests/Zrt/Application/BoostrapTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Zrt\Tests\Application;
+
+use Zend_Application_Bootstrap_Bootstrap,
+    Zrt\Application\Boostrap as ZrtBoostrap;
+
+class BoostrapTest extends Zend_Application_Bootstrap_Bootstrap
+{
+
+
+}
+
