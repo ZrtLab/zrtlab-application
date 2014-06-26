6ffe47c065ca94df4888c70f6b84f4fe3d5ae797
Luis Mayta <slovacus@gmail.com>
2014-06-26 12:37:45 -0500

    fixed site.php

--- provision/puppet/manifests/site.pp
+++ provision/puppet/manifests/site.pp
@@ -3,23 +3,30 @@ $domain_alias = "local.m.test-zend1.com"
 
 $mysqlPassword = ""
 
-Exec { path => "/usr/bin:/usr/sbin/:/bin:/sbin" }
+Exec { 
+    path => [
+            "/usr/bin", "/bin", "/usr/sbin", 
+            "/sbin", "/usr/local/bin", "/usr/local/sbin"
+            ]
+}
 
 exec { "apt-update":
     command => "apt-get update",
-    before  => Stage["main"],
+            before  => Stage["main"],
 }
 
 include mysql
 include git
 include wget
+include curl
+include sqlite
 include php
 include apache
 include vim
 
 wget::fetch { "get composer":
-       source      => 'https://getcomposer.org/composer.phar',
-       destination => '/var/www/src/composer.phar',
-       timeout     => 0,
-       verbose     => false,
-    }
+    source      => 'https://getcomposer.org/composer.phar',
+                destination => '/var/www/src/composer.phar',
+                timeout     => 0,
+                verbose     => false,
+}
