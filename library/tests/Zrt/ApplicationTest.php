dc1a767c63812746c857b663e102ca4ecd4b235d
Luis Mayta <slovacus@gmail.com>
2014-06-27 13:16:57 -0500

    fixed test instance initializer
    se encuentra un problema en el envio de archivos ini

--- library/tests/Zrt/ApplicationTest.php
+++ library/tests/Zrt/ApplicationTest.php
@@ -7,14 +7,16 @@ use PHPUnit_Framework_TestCase,
 
 class ApplicationTest extends PHPUnit_Framework_TestCase
 {
+    protected $inis = array();
 
     public function setUp()
     {
+        $this->inis[] = ROOT_PATH . "config/application.ini";
     }
 
     public function testInitializerInstance()
     {
-        $instance = new Zrt_Application();
+        $instance = new Zrt_Application(ENVIRONMENT,$this->inis);
         $this->assetNotNull($instance);
     }
 
